25639acecf05e8f5ecadec799276078a74087879
John Micoh Padin <padinjohnmicoh@gmail.com>
2026-09-23 12:00:33 +0800

    Add maximum late fee cap of 20 dollars - padin.johnmicoh

--- catalog.js
+++ catalog.js
@@ -3,7 +3,13 @@ function isValidLoan(daysLate) {
 }
 
 function calculateLateFee(daysLate, ratePerDay) {
-  return Math.floor(daysLate * ratePerDay);
+  let fee = Math.floor(daysLate * ratePerDay);
+
+  if (fee > 20) {
+    fee = 20;
+  }
+
+  return fee;
 }
 
 module.exports = { isValidLoan, calculateLateFee };
